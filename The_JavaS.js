62bc16c1e60a1e594f7e4e826c70d4a362bde1ce
RachAppweb <126121532+RachAppweb@users.noreply.github.com>
2023-03-29 22:27:25 +0000

    Update The_JavaS.js
    the close button

--- The_JavaS.js
+++ The_JavaS.js
@@ -41,7 +41,7 @@ function showAlert() {
       `<svg xmlns="http://www.w3.org/2000/svg" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:"style="width:20;height:20px">
       <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
     </svg><div>${message}</div>  `,
-      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      `  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`,
       "</div>",
     ].join("");
 
@@ -50,7 +50,7 @@ function showAlert() {
 
   alert(
     "Please chose a picture that contains 600/600 in it's dimentions",
-    "primary"
+    "success"
   );
 }
 
